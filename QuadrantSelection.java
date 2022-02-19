c355caccf8b9de6c8e8620573c2506e57cb16507
Edelio-Taboada <90776613+Edelio-Taboada@users.noreply.github.com>
2022-02-19 10:47:54 -0500

    Create QuadrantSelection.java

---
+++ QuadrantSelection.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class QuadrantSelection {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int x = inp.nextInt();
+        int y = inp.nextInt();
+        if(x>0 && y>0){
+            System.out.println("1");
+        }else if (x<0 && y>0){
+            System.out.println("2");
+        }else if (x<0 && y<0){
+            System.out.println("3");
+        }else if (x>0 && y<0){
+            System.out.println("4");
+        }
+    }
+
+}
